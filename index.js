63d257a8f7a9674f96f69f13aa6379621885e4cf
Gaurav Chaudhary <jaatsaab735@gmail.com>
2024-12-23 16:07:21 +0530

    d

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5001; // Use PORT from .env or default to 5001
 
 // Middleware for CORS
 // Allow the frontend origin
-const allowedOrigins = ['https://gauravchaudhary.online', 'http://localhost:5001', 'http://localhost:5173','https://bajaj-frontend-rouge-gamma.vercel.app/','https://bajaj-frontend-rousge-gamma.vercel.app','https://bajaj-backend-65s7.onrender.com/bfhl'];
+const allowedOrigins = ['https://gauravchaudhary.online', 'http://localhost:5001', 'http://localhost:5173','https://bajaj-frontend-rouge-gamma.vercel.app/','https://bajaj-frontend-rousge-gamma.vercel.app','https://bajaj-backend-65s7.onrender.com/bfhl','https://bajaj-frontend-voc5.vercel.app'];
 app.use(cors({
   origin: allowedOrigins,
   methods: 'GET, POST, OPTIONS',
